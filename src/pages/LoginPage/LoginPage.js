ef75ef20e847e0a123d308d7c7ee99767c5c1f52
RohSungKyun <songkyun1108@naver.com>
2024-05-27 23:28:16 +0900

    [feat] 로그인페이지 UI 구현

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,43 @@
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+import { useState } from "react";
 
 const LoginPage = () => {
-    return(
-        <div>Login</div>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleData = (e) => {
+    if(e.target.type === "email"){
+      setEmail(e.target.value);
+    } else if(e.target.type === "password"){
+      setPassword(e.target.value);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    // login hook
+  }
+
+
+  return(
+    <div className="Wrapper">
+      <form className="LoginForm" onSubmit={handleSubmit}>
+        <div className="TitleBoard">
+            <h1 className="Title">Login</h1>
+        </div>
+          <input className="Email" placeholder="이메일을 입력하세요" type="email" value={email} onChange={handleData} required/>
+        <div>
+          <input className="Password" placeholder="비밀번호를 입력하세요" type="password" value={password} onChange={handleData} required/>
+        </div>
+        <button className="LoginButton">로그인</button>
+      </form>
+      <div className="Switcher">
+        <Link to={"/signup"}>회원가입</Link>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage;
---
+++ src/pages/LoginPage/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+    border: 1px solid rgba(142, 142, 142, 0.5);
+
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    width: 60vw;
+    height: 50vh;
+    align-items: center;
+`
