e248f00428f365f761e64bb4a30b0464b953e168
RohSungKyun <songkyun1108@naver.com>
2024-05-31 01:33:05 +0900

    [feat] 메인페이지 게시판 추가

---
+++ src/components/BoardItem/BoardItem.js
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom"
+import "./BoardItem.css";
+
+const BoardItem = ({category, writer, title, place, isRecruit, created_at}) => {
+    const nav = useNavigate();
+    const recruitStatus = () => {
+        if(isRecruit === 1){
+            return "모집중"
+        } else if (isRecruit === 0){
+            return "모집완료"
+        }
+    }
+    const status = recruitStatus();
+    return (
+        <div className="boardItem" onClick={()=>nav(`/viewBoard`)}>
+            <div className="ItemWrapper">
+                <div className="columnBasic">
+                    {category}
+                </div>
+                <div className="columnBasic">
+                    {writer}
+                </div>
+                <div className="columnTitle">
+                    {title}
+                </div>
+                <div className="columnBasic">
+                    {place}
+                </div>
+                <div className="columnBasic">
+                    {status}
+                </div>
+                <div className="columnBasic">
+                    {new Date(created_at).toLocaleDateString()}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default BoardItem;
