55209a1db5399f4e95c3bb6069edd6f8e5995aee
pkyujin <luxoris02@sookmyung.ac.kr>
2024-06-22 13:02:04 +0900

    [feat]공지사항 생성 기능

--- src/pages/NoticeListPage/NoticeListPage.js
+++ src/pages/NoticeListPage/NoticeListPage.js
@@ -1,34 +1,12 @@
 import { Navbar } from "../../components/Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import "./NoticeListPage.css";
+import {useSelector} from "react-redux";
 
 const NoticeListPage = () => {
-
+    const NoticeData = useSelector(state => state.notice);
     const nav = useNavigate(); 
-    const mockData = [
-        {
-            id : 1,
-            title : "<필독> 함성 면접 스터디 기본 규칙 설명",
-            created_at: "2024.04.21"
-
-        },
-        {
-            id : 2,
-            title : "스터디 장소 및 5월 스터디 일정 안내합니다.",
-            created_at : "2024.04.30"
-        },
-        {
-            id : 3,
-            title : "스터디 요일 및 장소 투표",
-            created_at : "2024.04.19"
-        },
-        {
-            id : 4,
-            title : "1주차 스터디는 비대면으로 진행됩니다.",
-            created_at : "2024.04.13"
-        }
-        
-    ]
+    
 
     return(
         <div>
@@ -40,7 +18,7 @@ const NoticeListPage = () => {
 
           <div className="noticeList">
              <p className="notice">공지사항</p>
-             {mockData.map((item) => (
+             {NoticeData.map((item) => ( 
                  <div key={item.id} className="noticeItem" onClick={()=>nav(`/notice`)}>
                      <div className="noticeContent">
                          <p className="notice-title">{item.title}</p>
--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import BoardReducer from "./BoardReducer";
 import CommentReducer from "./CommentReducer";
+import NoticeReducer from "./NoticeReducer";
 
 const RootReducer = combineReducers({
     boards : BoardReducer,
     comments : CommentReducer,
+    notice : NoticeReducer,
 })
 
 export default RootReducer;
