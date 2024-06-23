f69171eabbd52d9c498f3d971ca51a34b4bdf358
RohSungKyun <songkyun1108@naver.com>
2024-06-23 19:41:55 +0900

    [feat] 토큰 자동 갱신

--- src/api/client.js
+++ src/api/client.js
@@ -6,18 +6,58 @@ const client = axios.create({
     headers: {"Content-Type": "application/json"}
 });
 
-// 인터셉터 설정
+// 요청 인터셉터 설정
 client.interceptors.request.use(
-    response => {
+    config => {
         // 요청 성공 시 특정 작업 수행
         const token = localStorage.getItem("token");
         if (token) {
-            response.headers.Authorization = {access: `${token}`};
+            config.headers.Authorization = {access: token};
         }
-        return response;
+        return config;
     },
     error => {
         // 요청 실패 시 특정 작업 수행
+        return Promise.reject(error);
+    }
+)
+
+export const refreshAccessToken = async () => {
+    try{
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        // 리프레쉬 토큰으로 요청 보내기
+        const response = await axios.post("path", {refreshToken})
+        const newAccessToken = response.data.accessToken;
+
+        // 새로 발급받은 토큰을 스토리지에 저장
+        localStorage.setItem("token", newAccessToken);
+
+        return newAccessToken;
+    } catch(error) {
+        throw error;
+    }
+
+}
+
+client.interceptors.response.use(
+    response => {
+        return response;
+    },
+    async (error) => {
+        const originalConfig = error.config; //기존에 수행하려고 했던 작업
+        if(error.response.status === 401){
+            try {
+                const newToken = await refreshAccessToken();
+                if (newToken){
+                    client.defaults.headers.common["Authorization"] = {access : newToken};
+                    originalConfig.headers["Authprization"] = {access : newToken};
+                    return client(originalConfig);
+                }
+            } catch (refreshError) {
+                throw refreshError;
+            }
+        }
         return Promise.reject(error);
     }
 )
