1acb2deb85abd96f3b571faa54cb0092cee71914
pkyujin <luxoris02@sookmyung.ac.kr>
2024-06-27 16:35:08 +0900

    [chore] 탈퇴 모달 스타일 수정

--- src/components/DeleteConfirm/DeleteConfirm.js
+++ src/components/DeleteConfirm/DeleteConfirm.js
@@ -5,7 +5,7 @@ const DeleteConfirm = ({ show, handleClose }) => {
   return (
     <div className={`modal ${show ? "show" : ""}`}>
     <div className="message-design">확인 메시지</div>
-      <div className="modal-content">
+      <div className="confirm-modal-content">
         <p>탈퇴되었습니다.</p>
         <button className="ok-button" onClick={handleClose}>확인</button>
       </div>
